0fbef18fb56ec5669020abfa7a736d5d7d54ae35
edsay12 <edvandearaujo2@hotmail.com>
2020-11-19 16:31:28 -0300

    concer

--- app/paineladm/paginas/includes/header.php
+++ app/paineladm/paginas/includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Edsay</title>
+  <title>AdmLuiz</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -18,12 +18,13 @@
   <link rel="stylesheet" href="app/paineladm/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
   <link rel="stylesheet" href="app/paineladm/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="app\assets\icomoon\icomoon.css.css">
 
   <!-- DataTables -->
   <link rel="stylesheet" href="app/paineladm/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="app/paineladm/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="app/paineladm/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-
+ 
   <!-- Theme style -->
   <link rel="stylesheet" href="app/paineladm/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
